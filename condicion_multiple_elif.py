8b3bc292cf84e9441a70d0d6da6b0b76497b3d3e
Pato700 <reateguipato@gmail.com>
2024-08-05 11:48:50 -0500

    inicial commit

---
+++ condicion_multiple_elif.py
@@ -0,0 +1,28 @@
+"""
+Este progrma convierte los numeros a letras.
+"""
+print("===============================")
+print("CONVERTIDOR DE NUMEROS A LETRAS")
+print("===============================")
+
+num = int(input("Ingrese el numero a convertir: "))
+
+if num == 1:
+    print('UNO')
+
+elif num == 2:
+    print('DOS')
+
+elif num == 3:
+    print('TRES')
+
+elif num == 4:
+    print('CUATRO')
+
+elif num == 5:
+    print('CINCO')
+
+else:
+    print("Este progrma solo puede convertir hasta el numero 5")
+
+print("Fin.")
