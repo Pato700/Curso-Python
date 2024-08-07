e68116d622cef8e7c0cf42bd9dbcee70cf1feef4
Pato700 <reateguipato@gmail.com>
2024-08-07 16:59:29 -0500

    condicion anidada V2

--- condicion_anidada.py
+++ condicion_anidada.py
@@ -1,55 +1,55 @@
 """
-Este programa convierte numeros a palabras y viseversa dependiendo la num que elijas.
+Este programa convierte numeros a palabras y viseversa dependiendo la opcion que elijas.
 """
 
 print("=========")
 print("CONVERSOR")
 print("=========")
 
-print("Menu de numes: \n")
+print("Menu de opciones: \n")
 print("Presiona 1 para convertir de numero a palabra.")
 print("Presiona 2 para convertir de palabra a numero. \n")
 
-num = int(input("Ingresar num: "))
+opcion = int(input("Ingresar opcion: "))
 
-if num == 1:
+if opcion == 1:
     print("\n Conversor de numero a palabra. \n")
 
-    num = int(input("Ingrese el numero a convertir: "))
+    opcion_uno = int(input("Ingrese el numero a convertir: "))
 
-    if num == 1:
+    if opcion_uno == 1:
         print('UNO')
-    elif num == 2:
+    elif opcion_uno == 2:
         print('DOS')
-    elif num == 3:
+    elif opcion_uno == 3:
         print('TRES')
-    elif num == 4:
+    elif opcion_uno == 4:
         print('CUATRO')
-    elif num == 5:
+    elif opcion_uno == 5:
         print('CINCO')
     else:
         print("Este programa solo puede convertir numeros del 1 al 5.")
 
-elif num == 2:
+elif opcion == 2:
     print("\n Conversor de palabra a numero. \n")
 
-    num = input("Ingrese la palabra a convertir: ")
-    num = num.lower()
+    opcion_dos = input("Ingrese la palabra a convertir: ")
+    opcion_dos = opcion_dos.lower()
 
-    if num == 'uno':
+    if opcion_dos == 'uno':
         print('1')
-    elif num == 'dos':
+    elif opcion_dos == 'dos':
         print('2')
-    elif num == 'tres':
+    elif opcion_dos == 'tres':
         print('3')
-    elif num == 'cuatro':
+    elif opcion_dos == 'cuatro':
         print('4')
-    elif num == 'cinco':
+    elif opcion_dos == 'cinco':
         print('5')
     else:
         print("Este programa solo puede convertir palabras (uno, dos, tres, cuatro y cinco).")
 
 else:
-    print("num no valida.")
+    print("Opcion no valida.")
 
 print("Fin.")
