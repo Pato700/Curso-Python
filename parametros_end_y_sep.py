dc9623668fa36c19bfccd937cd4fac7bb9f378f9
Pato700 <reateguipato@gmail.com>
2024-08-08 07:46:37 -0500

    parametro end y sep V1

---
+++ parametros_end_y_sep.py
@@ -0,0 +1,18 @@
+"""
+El parámetro end indica la continuación de la impresión en pantalla en una sila linea.
+El parámetro sep se utiliza para dar formato a las cadenas de caracteres ue deben imprimirse 
+en pantalla, agregando un separador entre las cadenas que se imprimirán.
+"""
+
+print("=============")
+print("PARÁMETRO END")
+print("=============")
+
+print("\nEsto es un", end="-*/")
+print("ejemplo\n")
+
+print("=============")
+print("PARÁMETRO SEP")
+print("=============")
+
+print("\n1", "2", "3", "4", "5", sep="-")
