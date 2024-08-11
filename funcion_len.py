8eee056886a30b9b6f48cb989431a2df4095a3c7
Pato700 <reateguipato@gmail.com>
2024-08-11 13:20:53 -0500

    funcion len V1

---
+++ funcion_len.py
@@ -0,0 +1,10 @@
+"""
+Este programa cuenta la longitud o números de caracteres de una palabra.
+"""
+
+#opción 1
+print("Hola tiene", len("Hola"), "caracteres")
+
+#opción 2
+longitud = len("Python es genial!")
+print("La palabra Python es genial! tiene", longitud, "caracteres")
