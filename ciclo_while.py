58722ec1854a38b36a6a8babb2a9e8654587dfe4
Pato700 <reateguipato@gmail.com>
2024-08-11 10:48:48 -0500

    ciclo while V1

---
+++ ciclo_while.py
@@ -0,0 +1,21 @@
+"""
+Este programa entra en un bucle hasta que la condición se cumpla.
+"""
+
+print("===========")
+print("BUCLE WHILE")
+print("===========")
+
+#Ejemplo 1
+x = 1
+while x < 3:
+    print(x)
+    x += 1
+print("Fin.\n")
+
+#Ejemplo 2
+x = 1
+while x < 11:
+    print("Patrick")
+    x += 1
+print("Fin.")
