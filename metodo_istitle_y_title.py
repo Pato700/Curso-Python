793b2018adafedd969e7a3d55327ae59dc68ba89
Pato700 <reateguipato@gmail.com>
2024-08-11 17:28:27 -0500

    metodo istitle y title V1

---
+++ metodo_istitle_y_title.py
@@ -0,0 +1,9 @@
+first_name = input("Nombre: ")
+last_name = input("Apellido: ")
+full_name = f"{first_name} {last_name}"
+
+print(full_name.istitle())
+
+print(full_name.title())
+
+print(full_name)
