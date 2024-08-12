f8e469fe6b492325a2a6d1420f986ad0c0b91340
Pato700 <reateguipato@gmail.com>
2024-08-12 04:11:47 -0500

    metodo swapcase V1

---
+++ metodo_swapcase.py
@@ -0,0 +1,21 @@
+"""
+El método swapcase invierte un string si esta en mayúscula a minúscula y viseversa.
+"""
+
+txt_lower_upper = 'pYTHON eS gENIAL'
+print(txt_lower_upper.swapcase())
+
+txt_upper = 'CADENA DE MAYUSCULA'
+print(txt_upper.swapcase())
+
+txt_lower = 'cadena de minuscula'
+print(txt_lower.swapcase())
+
+txt_number = '12345'
+print(txt_number.swapcase())
+
+print()
+print(txt_lower_upper)
+print(txt_upper)
+print(txt_lower)
+print(txt_number)
