c982be9baaa966bdae808919db1a84eb091c1a14
Pato700 <reateguipato@gmail.com>
2024-08-12 13:51:25 -0500

    metodo capitalize V1

---
+++ metodo_capitalize.py
@@ -0,0 +1,9 @@
+"""
+El método capitalize se utiliza para convertir el primer carácter de un string en mayúscula, 
+y a su vez, todas las letras restantes en minúsculas.
+"""
+
+string = 'el VIAJE es la Recompensa'
+print(f"Antes de aplicar capitalize(): {string}")
+string = string.capitalize()
+print(f"Después de aplicar capitalize(): {string}")
