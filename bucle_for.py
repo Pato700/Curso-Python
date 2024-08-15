649172e7b5ecfa3d86e5a32f24118925d381d4b2
Pato700 <reateguipato@gmail.com>
2024-08-14 23:44:09 -0500

    bucle for V1

---
+++ bucle_for.py
@@ -0,0 +1,11 @@
+"""
+Es una estructura de control que nos permite repetir un bloque de instrucciones (sentencias),
+cierta cantidad de veces.
+"""
+
+string = 'Hola Patrick'
+
+for character in string:
+    print(character)
+
+print("Fin del programa.")
