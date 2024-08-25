930aedf15fdd4bd356f80803fe349e711f145a65
Pato700 <reateguipato@gmail.com>
2024-08-25 17:35:52 -0500

    listas V1

---
+++ listas.py
@@ -0,0 +1,23 @@
+print("Lista vacia")
+
+lista_vacia = []
+print(lista_vacia)
+
+print("\nListas homogeneas")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(vocales)
+
+numeros_enteros = [1, 2, 3, 4, 5]
+print(numeros_enteros)
+
+numeros_decimales = [1.5, 2.2, 3.3, 4.5, 5.2]
+print(numeros_decimales)
+
+valores_booleanos = [True, False, False, True]
+print(valores_booleanos)
+
+print("\nListas hererogeneas")
+
+datos = ["Patrick", 20, 1.72, True]
+print(datos)
