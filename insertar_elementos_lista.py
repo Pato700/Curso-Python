b92fc20c0ed6f19c444891d8ea71a3cc19f41e01
Pato700 <reateguipato@gmail.com>
2024-09-04 16:48:28 -0500

    insertar elementos lista V1

---
+++ insertar_elementos_lista.py
@@ -0,0 +1,19 @@
+print("Lista original")
+letters = ["b", "d", "f", "g"]
+print(f"Lista: {letters}")
+
+print("\nInsertando 'a' en posicion 0")
+letters.insert(0, "a")
+print(f"Lista: {letters}")
+
+print("\nInsertando 'c' en posicion 0")
+letters.insert(2, "c")
+print(f"Lista: {letters}")
+
+print("\nInsertando 'e' en posicion 0")
+letters.insert(4, "e")
+print(f"Lista: {letters}")
+
+print("\nInsertando 'z' en posicion 0")
+letters.insert(100, "z")
+print(f"Lista: {letters}")
