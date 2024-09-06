dc83134a07c16625b10e3bbf8671961baef853f0
Pato700 <reateguipato@gmail.com>
2024-09-06 11:01:21 -0500

    eliminar elementos lista V1

---
+++ eliminar_elementos_lista.py
@@ -0,0 +1,24 @@
+vocales = ["a", "e", "i", "o", "u"]
+print(f"LIsta: {vocales}")
+print(f"Elemento eliminado: {vocales.pop()}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLIsta: {vocales}")
+print(f"Elemento eliminado en la posicion 2: {vocales.pop(2)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLIsta: {vocales}")
+print(f"Elemento eliminado en la posicion 0: {vocales.pop(0)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLIsta: {vocales}")
+print(f"Elemento eliminado en la posicion -2: {vocales.pop(-2)}")
+print(f"Lista: {vocales}")
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"\nLIsta: {vocales}")
+print(f"Elemento eliminado en la posicion 5: {vocales.pop(5)}")
+print(f"Lista: {vocales}")
