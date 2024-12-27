11bf09694744609ff15a2a6b61c6d77323ebee00
Pato700 <162893235+Pato700@users.noreply.github.com>
2024-12-27 01:28:35 -0500

    metodo reverse V1

---
+++ metodo_reverse.py
@@ -0,0 +1,16 @@
+"""
+El método reverse se encarga de invertir los elementos de una lista en su mismo lugar,
+modifica las pocisiones de la lista, en lugar de crear una nueva lista
+"""
+
+vocales = ["a", "e", "i", "o", "u"]
+print(f"Lista: {vocales}")
+
+vocales.reverse()
+print(f"\nLista: {vocales}")
+print(f"Lista: {vocales}")
+print(f"pocision 0: {vocales[0]}")
+
+vocales.reverse()
+print(f"\nLista: {vocales}")  
+print(f"pocision 0: {vocales[0]}")
